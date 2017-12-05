a0704f24b9b6e3b5c124347a3c6aa4ccc298fce0
Evan  Markert <evanmarkert@gmail.com>
2017-12-05 15:33:19 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,7 +4,7 @@ end
 
 
 def shipping_manifest
-  the_manifest = {"whale bone corsets" => "5", "porcelain vases" => "2", "oil paintings" => "3"}
+  the_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 end
 
 def retrieval
@@ -23,8 +23,8 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-  shipping_manifest["muskets"] = "2"
-  shipping_manifest["gun powder"] = "4"
+  shipping_manifest["muskets"] = 2
+  shipping_manifest["gun powder"] = 4
   shipping_manifest
 
 end
